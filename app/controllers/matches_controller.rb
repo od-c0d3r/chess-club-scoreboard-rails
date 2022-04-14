d8d96f4028856dc5fcd6ce44cea9489b72f62f06
od-c0d3r <omar_rashad@aol.com>
2022-04-14 23:27:12 +0200

    Create: init matches controllers actions and new view

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,23 @@
+class MatchesController < ApplicationController
+  def index
+    @matches = Match.all
+  end
+
+  def new
+    @match = Match.new
+  end
+
+  def create
+    @match = Match.new(match_params)
+
+    respond_to do |format|
+      if @match.save
+        format.html { redirect_to users_path, notice: "Ranks updated and match was successfully recorded." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @match.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-
+  resources :matches, only: [:index, :new, :create]
+  
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
