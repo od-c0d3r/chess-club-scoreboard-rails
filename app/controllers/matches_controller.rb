149d2e9b36df5817914281ae3a6d825cf6fae5d5
od-c0d3r <omar_rashad@aol.com>
2022-04-15 15:05:11 +0200

    Model: add match validations

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -12,8 +12,10 @@ class MatchesController < ApplicationController
     player_one = User.find(match_params["player_one"])
     player_two = User.find(match_params["player_two"])
     result = match_params["result"]
-    @match = Match.new(player_one:player_one, player_two:player_two, result:result)
     
+    @match = Match.new(player_one:player_one, player_two:player_two, result:result)
+    @users = User.all
+
     respond_to do |format|
       if @match.save
         # update_rank(player_one, player_two, result, @match)
@@ -21,6 +23,7 @@ class MatchesController < ApplicationController
         format.html { redirect_to users_path, notice: "Ranks updated and match successfully recorded." }
         format.json { render :show, status: :created, location: @match }
       else
+        debugger
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @match.errors, status: :unprocessable_entity }
       end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,9 @@
 class Match < ApplicationRecord
+  validates_presence_of :player_one
+  validates_presence_of :player_two
+  validates_presence_of :result
+  validates :player_one, comparison: { other_than: :player_two, message: "and Player two should be different" }
+
   belongs_to :player_one, class_name: 'User'
   belongs_to :player_two, class_name: 'User'
 end
